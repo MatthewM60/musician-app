8f107c9ccaa457f81594e3669559f53f10791834
MatthewM60 <112290489+MatthewM60@users.noreply.github.com>
2025-03-27 02:57:39 -0700

    Base Database Implementation

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const path = require('path');
-const store = require('./store/datastore');
-const initialStoreData = require('./store/data');
 const Musician = require('./models/musician');
 const musicianRoutes = require('./routes/musician');
+const mysql = require('mysql2/promise');
 
 const app = express();
 const port = process.env.PORT || 3001;
 
+// Database configuration
+const dbConfig = {
+  host: process.env.RDS_HOSTNAME || 'your-rds-endpoint',
+  user: process.env.RDS_USERNAME || 'your-username',
+  password: process.env.RDS_PASSWORD || 'your-password',
+  database: process.env.RDS_DB_NAME || 'your-database',
+  port: process.env.RDS_PORT || 3306
+};
+
 // include routes
 app.use('/musician', musicianRoutes);
-
 app.use(express.static('public'));
 
 // Index route
@@ -18,15 +25,25 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/build/index.html'));
 });
 
-// initialize store
-const musician = new Musician(store);
-musician.initStore(initialStoreData);
-app.locals.musician = musician;
+// Initialize database and musician model
+async function initializeApp() {
+  try {
+    const connection = await mysql.createConnection(dbConfig);
+    const musician = new Musician(connection);
+    app.locals.musician = musician;
+    
+    // start server
+    const server = app.listen(port, () => {
+      console.log("Server started on port " + port);
+    });
+    
+    return server;
+  } catch (error) {
+    console.error('Failed to initialize app:', error);
+    process.exit(1);
+  }
+}
 
-// start server
-const server = app.listen(port, () => {
-  console.log("Server started on port " + port);
-});
+initializeApp();
 
-module.exports = server;
-// test
+module.exports = app;
--- models/musician.js
+++ models/musician.js
@@ -1,66 +1,54 @@
-// model object with methods for store CRUD operations
 class Musician {
-
-  constructor(store) {
-    this.store = store;
-  }
-  
-  // hydrate store with initial data
-  initStore(data) {
-    const newStore = Object.assign(this.store, data);
-    this.store = newStore;
-  };
-
-  // utility functions
-  getStore() {
-    return this.store;
-  }
-  
-  printStore() {
-    console.log(this.store);
-  };
-
-  isMusicianInStore(id) {
-    const keys = Object.keys(this.store);
-    return keys.includes(id);
+  constructor(dbConnection) {
+    this.db = dbConnection;
   }
 
-  // get list of musicians from storage
-  getMusicians(id, callback) {
-    return callback(null, this.store);
-  }
-
-  // get musician from storage
-  getMusician(id, callback) {
-    if(this.isMusicianInStore(id)) {
-      return callback(null, this.store[id]);
+  async getMusicians(callback) {
+    try {
+      const [rows] = await this.db.execute('SELECT * FROM musicians');
+      callback(null, rows);
+    } catch (err) {
+      callback(err, null);
     }
-    return callback('Musician does not exist');
   }
 
-  // modify existing musician or add a new one to storage
-  putMusician(id, musician, callback) {
-    if (id !== musician.firstName.toLowerCase()) {
-      return callback("Musician id in request path and body do not match.");
+  async getMusician(id, callback) {
+    try {
+      const [rows] = await this.db.execute(
+        'SELECT * FROM musicians WHERE id = ?',
+        [id]
+      );
+      callback(null, rows[0] || null);
+    } catch (err) {
+      callback(err, null);
     }
-    const newStore = Object.assign({}, this.store);
-    if(this.isMusicianInStore(id)) {
-      const newMusician = Object.assign(this.store[id], musician);
-      newStore[id] = newMusician;
-    }else {
-      newStore[id] = musician;
-    }
-    this.store = newStore;
-    return callback(null, id);
   }
 
-  deleteMusician(id, callback) {
-    const newStore = Object.assign({}, this.store);
-    delete newStore[id];
-    this.store = newStore;
-    return callback(null, id);
+  async putMusician(id, data, callback) {
+    try {
+      const [result] = await this.db.execute(
+        'INSERT INTO musicians (id, first_name, last_name, genre) VALUES (?, ?, ?, ?) ' +
+        'ON DUPLICATE KEY UPDATE first_name = ?, last_name = ?, genre = ?',
+        [id, data.firstName, data.lastName, data.genre, 
+         data.firstName, data.lastName, data.genre]
+      );
+      callback(null, id);
+    } catch (err) {
+      callback(err, null);
+    }
   }
-  
+
+  async deleteMusician(id, callback) {
+    try {
+      const [result] = await this.db.execute(
+        'DELETE FROM musicians WHERE id = ?',
+        [id]
+      );
+      callback(null, id);
+    } catch (err) {
+      callback(err, null);
+    }
+  }
 }
 
 module.exports = Musician;
